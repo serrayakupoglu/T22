c620c0ab180b2be37f246215d1a527a67fea25d9
kmlayhn <kemal.ayhan@sabanciuniv.edu>
2023-11-01 14:31:25 +0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:untitled1/src/features/pages/first_page.dart';
+import 'src/features/pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF003049), // Custom primary color
+        fontFamily: 'Montserrat',        // Custom font
+      ),
+      home: FirstPage(),
+    );
+  }
+}
