abe3a58dae0d8c1620ebdfc5c7404b1a1bc28d86
bbcoskun <129174639+bbcoskun@users.noreply.github.com>
2023-11-01 16:48:53 +0300

    Update signup.dart

---
+++ lib/src/features/pages/signup.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      resizeToAvoidBottomInset: false,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(15, 110, 0, 0),
+                    child: Text("SignUp",
+                        style: TextStyle(
+                            fontSize: 40, fontWeight: FontWeight.bold
+                        )
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 35, left: 20, right: 30),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    //controller: _emailController,
+                    decoration: InputDecoration(
+                        labelText: 'EMAIL',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green),
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  TextField(
+                    //controller: _passwordController,
+                    decoration: InputDecoration(
+                        labelText: 'PASSWORD',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green),
+                        )
+                    ),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 5.0,),
+                  SizedBox(height: 40,),
+                  Container(
+                    height: 40,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20),
+                      shadowColor: Colors.greenAccent,
+                      color: Colors.black,
+                      elevation: 7,
+                      child: GestureDetector(
+                          onTap: () async{
+                           // _register();
+                          },
+                          child: Center(
+                              child: Text(
+                                  'SIGNUP',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat'
+                                  )
+                              )
+                          )
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      InkWell(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(
+                            'Go Back',
+                            style: TextStyle(
+                                color: Colors.blueGrey,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.underline
+                            )
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        )
+    );
+  }
+}
