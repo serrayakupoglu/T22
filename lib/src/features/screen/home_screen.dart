bde3fcec114bcb09dd864040d113c48d57dee1d0
kmlayhn <kemal.ayhan@sabanciuniv.edu>
2024-01-22 18:26:26 +0300

    home screen update

--- lib/src/features/screen/home_screen.dart
+++ lib/src/features/screen/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:untitled1/src/features/controller/song_controller.dart';
 import 'package:untitled1/src/features/controller/user_controller.dart';
 import 'package:untitled1/src/features/models/recommendations_cache.dart';
 import 'package:untitled1/src/features/models/recommended_song.dart';
+import 'package:untitled1/src/features/service/storage_service.dart';
 import '../models/friend_recommended_song.dart';
 import '../models/playlist_recommendation_instance.dart';
 
@@ -20,6 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
   late SongController songController;
   late Future<RecommendedSong?> song;
   late Future<FriendSong?> friendSong;
+  late Future<String?> username;
   late Future<List<PlaylistRecommendation>> relaxingPlaylist;
   late Future<List<PlaylistRecommendation>> energeticPlaylist;
   late Future<List<PlaylistRecommendation>> playlist;
@@ -34,7 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void fetchData () async {
-
+    username = StorageService().readSecureData('username');
     if(!RecommendationsCache.containsRecommendation('recommendedSong')) {
       song = userController.recommendSong();
       RecommendationsCache.setRecommendation('recommendedSong', song);
@@ -86,79 +88,178 @@ class _HomeScreenState extends State<HomeScreen> {
         setState(() {});
       },
       child: SingleChildScrollView(
-
-        child: Column(
-          children: [
-            FutureBuilder<RecommendedSong?>(
-              future: song,
-              builder: (BuildContext context,
-                  AsyncSnapshot<RecommendedSong?> snapshot) {
-                if (snapshot.hasData && snapshot.data != null) {
-                  final recommendedSong = snapshot.data!;
-                  return Container(
-                    child: Column(
-                      children: [
-                        SizedBox(height: 10,),
-                        BodyText(msg: "From Your Recent Likes:"),
-                        SizedBox(height: 10), // Add space between texts
-                        Text(
-                          recommendedSong.recommendedSong == "" ? "You need To Like a Song" : recommendedSong.recommendedSong,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue, // Customize text color
-                          ),
+        physics: const AlwaysScrollableScrollPhysics(),
+        child:SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FutureBuilder<String?>(
+                future: username,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && snapshot.data != null) {
+                    return Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Text(
+                        'Hello ${snapshot.data},\nwhat do you want to listen to today?',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green[700],
                         ),
-                        SizedBox(height: 10,)
-                      ],
-                    ),
-                  );
-                } else {
-                  return Container();
-                }
-              },
-            ),
-            FutureBuilder<FriendSong?>(
-              future: friendSong,
-              builder: (BuildContext context,
-                  AsyncSnapshot<FriendSong?> snapshot) {
-                if (snapshot.hasData && snapshot.data != null) {
-                  final friendSong = snapshot.data!;
-                  return Container(
-                    child: Column(
-
-                      children: [
-                        BodyText(msg: "From Your Friend @${friendSong.friend}:"),
-                        SizedBox(height: 10), // Add space between texts
-                        Text(
-                          friendSong.added == "" ? "You Need To Follow At Least One Person": friendSong.added,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue, // Customize text color
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                } else {
-                  return Container();
-                }
-              },
-            ),
-            GestureDetector(
-              child: Text('playlist'),
-              onTap: () {
-                userController.navigateToRecommendedPlaylistPage(context, 'playlist');
-              },
-            ),
-
-
-          ],
+                      ),
+                    );
+                  } else {
+                    return Container(); // Or some other placeholder
+                  }
+                },
+              ),
+              sectionTitle("From Your Recent Likes", Icons.favorite),
+              FutureBuilder<RecommendedSong?>(
+                future: song,
+                builder: (BuildContext context, AsyncSnapshot<RecommendedSong?> snapshot) {
+                  if (snapshot.hasData && snapshot.data != null) {
+                    final recommendedSong = snapshot.data!;
+                    return buildRecommendedSongSection(recommendedSong);
+                  } else {
+                    return Container(); // Or some other placeholder
+                  }
+                },
+              ),
+              sectionTitle("From Your Friend", Icons.group),
+              FutureBuilder<FriendSong?>(
+                future: friendSong,
+                builder: (BuildContext context, AsyncSnapshot<FriendSong?> snapshot) {
+                  if (snapshot.hasData && snapshot.data != null) {
+                    final friendSong = snapshot.data!;
+                    return buildFriendSongSection(friendSong);
+                  } else {
+                    return Container(); // Or some other placeholder
+                  }
+                },
+              ),
+              sectionTitle("For Your Mood", Icons.sentiment_very_satisfied),
+              moodPlaylistSection("Energetic", Icons.flash_on, () {userController.navigateToRecommendedPlaylistPage(context, 'energeticPlaylist');}),
+              moodPlaylistSection("Relaxing", Icons.spa, () {userController.navigateToRecommendedPlaylistPage(context, 'relaxingPlaylist');}),
+              sectionTitle("From Your Liked Songs", Icons.music_note),
+              buildLikedSongsSection(context),
+            ],
+          ),
         ),
+
       ),
     );
   }
+
+  Widget sectionTitle(String title, IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.green[700], size: 20),
+          const SizedBox(width: 8),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.green[700],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildRecommendedSongSection(RecommendedSong recommendedSong) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, bottom: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const BodyText(msg: "Song:"),
+          Text(
+            recommendedSong.recommendedSong == ""
+                ? "You need To Like a Song"
+                : recommendedSong.recommendedSong,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.white70,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildFriendSongSection(FriendSong friendSong) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, bottom: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          BodyText(msg: "Recommended by @${friendSong.friend}:"),
+          Text(
+            friendSong.added == ""
+                ? "You Need To Follow At Least One Person"
+                : friendSong.added,
+            style: const TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              color: Colors.white70,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget moodPlaylistSection(String title, IconData icon, VoidCallback onTap) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: ListTile(
+        leading: Icon(icon, color: Colors.green[700]),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.white70,
+          ),
+        ),
+        onTap: onTap,
+      ),
+    );
+  }
+
+
+  Widget buildLikedSongsSection(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, bottom: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: () {
+              // Replace 'Your Playlist Name' with the actual playlist name you want to pass
+              userController.navigateToRecommendedPlaylistPage(context, 'playlist');
+            },
+            child: const Text(
+              "Recommended playlist based on your liked songs!",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue, // Change the color to your preference
+                decoration: TextDecoration.underline, // Optional underline
+              ),
+            ),
+          ),
+          // You can include other widgets here
+        ],
+      ),
+    );
+  }
+
 }
 
 
